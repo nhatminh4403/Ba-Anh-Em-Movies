40b524d3182c4a92f1343297177a9dcda80bf971
Bui Chi Bao <buichibao1601@gmail.com>
2024-06-18 19:49:01 +0700

    update categorycontroller

--- src/main/java/com/example/movietickets/demo/service/CategoryService.java
+++ src/main/java/com/example/movietickets/demo/service/CategoryService.java
@@ -1,9 +1,47 @@
 package com.example.movietickets.demo.service;
 
 
+import com.example.movietickets.demo.model.Category;
+import com.example.movietickets.demo.repository.CategoryRepository;
+import jakarta.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.Optional;
 
+@RequiredArgsConstructor
+@Transactional
 @Service
 public class CategoryService {
+    private final CategoryRepository catologyRepository;
+
+    public List<Category> getAllCatologies(){
+        return catologyRepository.findAll();
+    }
+
+    public Optional<Category> getCategoryById(Long id) {
+        return catologyRepository.findById(id);
+    }
+
+    public void addCategory(Category category) {
+        catologyRepository.save(category);
+    }
+
+    public void updateCategory(@NotNull Category category) {
+        Category existingCategory = catologyRepository.findById((long)
+                        category.getId())
+                .orElseThrow(() -> new IllegalStateException("Category with ID " +
+                        category.getId() + " does not exist."));
+        existingCategory.setName(category.getName());
+        catologyRepository.save(existingCategory);
+    }
+
+    public void deleteCategoryById(Long id) {
+        if (!catologyRepository.existsById(id)) {
+            throw new IllegalStateException("Category with ID " + id + " does not exist.");
+        }
+        catologyRepository.deleteById(id);
+    }
 }
